5283a1f6cb4d438debb90fd440d9bde360457993
benjamin kisenge <benkisenge03@gmail.com>
2022-12-02 21:11:49 +0200

    Update methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
     has_many :likes
     belongs_to  :author, class_name: 'User'
 
-    after_save: update_posts_counter
+    after_save :update_posts_counter
 
     def update_posts_counter
         author.update(posts_counter: author.posts.size)
