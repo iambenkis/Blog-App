5d5d3049e70ca6e2e0ae3af65c99a3d0fd8c46af
benjamin kisenge <benkisenge03@gmail.com>
2022-12-02 21:07:10 +0200

    Create like method

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,10 @@ class Post < ApplicationRecord
     has_many :likes
     belongs_to  :author, class_name: 'User'
 
+    after_save: update_posts_counter
+
     def update_posts_counter
-        posts_counter += 1
+        author.update(posts_counter: author.posts.size)
     end
 
     def recent_comments
