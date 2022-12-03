f1bc2c695444361d83ae9991a7038048d0d7d8fd
benjamin kisenge <benkisenge03@gmail.com>
2022-12-03 16:59:30 +0200

    Fix rubocop linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,19 @@
 class Post < ApplicationRecord
-    has_many :comments
-    has_many :likes
-    belongs_to  :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
+  belongs_to :author, class_name: 'User'
 
-    after_save :update_posts_counter
+  after_save :update_posts_counter
 
-    validates :title, presence: true, allow_blank: false, allow_nil: false, length: { maximum: 250 }
-    validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-    validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, presence: true, allow_blank: false, allow_nil: false, length: { maximum: 250 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-    def update_posts_counter
-        author.update(posts_counter: author.posts.size)
-    end
+  def update_posts_counter
+    author.update(posts_counter: author.posts.size)
+  end
 
-    def recent_comments
-        comments.order(created_at: :desc).limit(5)
-    end
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
