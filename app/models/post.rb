74ff1105b187b886ab8deec567237339c25d01b7
benjamin kisenge <benkisenge03@gmail.com>
2022-12-03 14:25:21 +0200

    Add validate to post and user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,15 +1,19 @@
 class Post < ApplicationRecord
-  has_many :comments
-  has_many :likes
-  belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+    belongs_to  :author, class_name: 'User'
 
-  after_save :update_posts_counter
+    after_save :update_posts_counter
 
-  def update_posts_counter
-    author.update(posts_counter: author.posts.size)
-  end
+    validates :title, presence: true, allow_blank: false, allow_nil: false, length: { maximum: 250 }
+    validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
+    def update_posts_counter
+        author.update(posts_counter: author.posts.size)
+    end
+
+    def recent_comments
+        comments.order(createdn_at: :desc).limit(5)
+    end
 end
