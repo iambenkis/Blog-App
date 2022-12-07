6093f3ff6fa895e722f8cbe10112d327a9c49f71
benjamin kisenge <benkisenge03@gmail.com>
2022-12-07 12:21:40 +0200

    Install rails-dom-testing

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,9 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe 'Posts', type: :controller do
-  describe 'GET index' do
-    it "is a success" do
-      expect(response).to have_http_status(:ok)
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    before :each do
+      get '/users/:user_id/posts/'
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render index' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'should include correct placeholder' do
+      expect(response.body).to include("Here are ALL Posts for a given user you are in '/users/user_id/posts'")
     end
   end
 end
+
+# RSpec.describe PostsController, type: :controller do
+#   describe "GET posts#index" do
+
+#   end
+# end
