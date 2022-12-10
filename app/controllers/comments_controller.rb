b0b23a6e9d7a296f04b3a247ade33383257c2da3
benjamin kisenge <benkisenge03@gmail.com>
2022-12-11 00:06:07 +0200

    Add comment concern

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,20 @@
 class CommentsController < ApplicationController
-  def new; end
+  def new
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
+  end
 
-  def create; end
+  def create
+    post = Post.find(params[:post_id])
+    comment = Comment.create(author: current_user, post:, **comment_params)
+    return unless comment.save
+
+    redirect_to user_post_path(current_user.id, post.id), notice: 'Comment saved'
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
